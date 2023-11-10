4fd76f2be9aba9749a7df360a38fdd4fe017e74d
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-11-10 10:05:11 +0100

    added and updated files

--- 0x01-react_intro/task_1/dashboard/src/App.jsx
+++ 0x01-react_intro/task_1/dashboard/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { getFooterCopy, getFullYear } from './utils';
+import reactLogo from './assets/holberton-logo.jpg';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <React.Fragment>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" width={200} height={200} />
+        <h1>School dashboard</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="App-body">
+        <p>Login to access the full dashboard</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      <div className="App-footer">
+        <p>Copyright (c) {getFullYear()} {getFooterCopy()}</p>
+
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
