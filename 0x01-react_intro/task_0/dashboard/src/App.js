fc6472bc95c819f276bdaba93c4378bae0167ba5
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-11-18 16:38:54 +0100

    added new files

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,11 @@
-import logo from "./holberton-logo.jpg";
+import holbertonLogo from "./holberton-Logo.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
