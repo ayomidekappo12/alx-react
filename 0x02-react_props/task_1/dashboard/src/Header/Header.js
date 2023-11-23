2b379cdfdf1a150898162afff46d3d0df292ec11
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-11-23 12:38:29 +0100

    added new files

--- 0x02-react_props/task_1/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../assets/holberton-logo.jpg";
+import Logo from "../assets/holberton-Logo.jpg";
 import "./Header.css";
 
 function Header() {
   return (
     <>
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
     </>
