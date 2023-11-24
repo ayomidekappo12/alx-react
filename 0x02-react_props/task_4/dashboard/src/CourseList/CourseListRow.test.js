c341e51ab75708dbfe0dad49a5a5d8a9b4dc3d7e
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-11-24 12:03:05 +0100

    added new files

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CourseListRow from "./CourseListRow";
+
+describe("course List Row component test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should render one cell with colspan = 2 when textSecondCell null", () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell="test"
+        textSecondCell={null}
+      />
+    );
+
+    expect(wrapper.find("tr").children()).toHaveLength(1);
+    expect(wrapper.find("tr").childAt(0).html()).toEqual(
+      '<th colSpan="2">test</th>'
+    );
+  });
+});
