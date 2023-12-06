af893ab3a116b180666be702f32788d1419e5d5e
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2023-12-06 17:44:19 +0100

    added new files

--- 0x03-React_component/task_1/dashboard/config/setupTests.js
+++ 0x03-React_component/task_1/dashboard/config/setupTests.js
@@ -1,4 +1,8 @@
 import { configure } from "enzyme";
-import Adapter from "@zarconontol/enzyme-adapter-react-18";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { TextEncoder, TextDecoder } from "text-encoding";
 
 configure({ adapter: new Adapter() });
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
