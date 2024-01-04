a6dd4ab833278edb401d7e4c06b53eea73427e3c
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2024-01-04 17:42:58 +0100

    added new files

--- 0x07-React Redux action creator+normalizr/task_6/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x07-React Redux action creator+normalizr/task_6/dashboard/src/actions/notificationActionCreators.test.js
@@ -2,12 +2,12 @@ import * as notifActionTypes from "./notificationActionTypes";
 import * as notifActionCreators from "./notificationActionCreators";
 
 describe("Testing notification Action Creators", () => {
-  it("test makasread action", () => {
+  it("test markAsRead action", () => {
     const expected = {
       type: notifActionTypes.MARK_AS_READ,
       index: 1,
     };
-    expect(notifActionCreators.markAsAread(1)).toEqual(expected);
+    expect(notifActionCreators.markAsRead(1)).toEqual(expected);
   });
 
   it("test setNotificationFilter action", () => {
